d5a9e893ad6123bc26761bef4b7a2d80d8d1e029
Emrix <mgizmor37@gmail.com>
2017-10-20 20:17:29 +0000

    Working Copy of Code without networking.

--- main.cpp
+++ main.cpp
@@ -3,30 +3,8 @@
 #include <iostream>
 #include <ctime>
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<strings.h>
-#include<string.h>
-#include<arpa/inet.h>
-
-#define ERROR -1
-#define BUFFER 1024
-
-	struct sockaddr_in remote_server;
-	int sock; //Socket ID
-	char input[BUFFER]; //User input
-	char output[BUFFER]; //Sent from the server
-	int len;
-
-
-
 using namespace std;
 
-string lastInputUpdate;
 //These numbers refer to the WIRINGPI numbering scheme, NOT the sapphire nor physical numbering schemes
 const int ADDRESS_PIN_ARRAY[] = { 8, 7, 1, 15, 9 };
 const int ADDRESS_ARRAY_SIZE = 5;//This will tell us how many we use in the program
@@ -101,32 +79,10 @@ void outputPinReset() {
 
 //Reads From Output Cache
 void checkOutputCache(char(&lastOutputCacheTime)[TIMESTAMP_SIZE], char(&cachedOutputValues)[TOTAL_MODULES][OUTPUT_ARRAY_SIZE]) {
-/*	char currentOutputCacheTime[TIMESTAMP_SIZE];
-	ifstream in_file("Output Cache.txt");//Open the file
-	//Check the timestamp
-	bool cacheIsUpdated = false;
-	in_file.get(currentOutputCacheTime, TIMESTAMP_SIZE);
-	for (int x = 0;x < TIMESTAMP_SIZE;x++) {
-		if (lastOutputCacheTime[x] != currentOutputCacheTime[x]) { //Looping through each of the chars in the array to see if there is a difference
-			cacheIsUpdated = true;//if the values are not the same, the cache has been updated
-			lastOutputCacheTime[x] = currentOutputCacheTime[x];
-		}
-	}
-	if (cacheIsUpdated) { //if the chache has been updated, update the cache values
-		char c;
-		char d[20];
-		for (int x = 0;x < TOTAL_MODULES;x++) { //Loop through each of the modules and update the values
-			in_file.get(c);//Get rid of the newline character
-			in_file.get(cachedOutputValues[x], OUTPUT_ARRAY_SIZE);//Put the char array with the specific module
-			in_file.get(d, 20);//get rid of anything else that's on this line
-		}
-	}
-	in_file.close();
-*/
 	for (int x = 0; x < TOTAL_MODULES; x++) {
 		for (int y = 0; y < OUTPUT_ARRAY_SIZE;y++) {
-//			if (y == 2 || y == 3) {
-			if (y == 4 || y == 5) {
+			if (y == 2 || y == 3 || y == 5) { //BLUE AND GREEN AND WHITE
+//			if (y == 4 || y == 5) { //RED AND WHITE
 				cachedOutputValues[x][y] = '1';
 			} else {
 				cachedOutputValues[x][y] = '0';
@@ -150,16 +106,13 @@ void setOutputPins(int moduleNumber, int outputAmountInt, char outputArray[][OUT
 //Writes Input Cache
 void setInputCache(bool(&cached)[TOTAL_MODULES][INPUT_ARRAY_SIZE]) {
 	ofstream out_file("/var/www/html/index.html");
-//	out_file.open("output.txt");
 	out_file << "<!DOCTYPE html>\n  <head>\n  </head>\n  <body>\n";
 	out_file << time(NULL) << endl;
 	for (int y = 0;y < TOTAL_MODULES;y++) {
 		for (int x = 0;x < INPUT_ARRAY_SIZE;x++) {
 			out_file << cached[y][x];
-//			cout << cached[y][x];
 		}
 		out_file << "\n";
-//		cout << "\n";
 	}
 	out_file << "  </body>\n</html>";
 	out_file.close();
@@ -177,19 +130,6 @@ void checkInputPins(int moduleNumber, bool(&cachedInputValues)[TOTAL_MODULES][IN
 
 //Now we are actually going to get to our actual function
 int main(void) {
-//Network Setup
-//Insert something here to resolve the voyager-core.local address through DNS or mDNS
-	if((sock = socket(AF_INET, SOCK_STREAM, 0)) == ERROR) { //attempt to create socket
-		perror("socket");
-		//exit(-1);
-	}
-
-	remote_server.sin_family = AF_INET; //fill out remote_server information
-	remote_server.sin_port = htons(atoi("50000"));
-	remote_server.sin_addr.s_addr = inet_addr("10.1.13.176");
-	bzero(remote_server.sin_zero, 8);
-
-
 	//Pin Setup
 	wiringPiSetup();
 	pinSetup();
