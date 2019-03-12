98046ed5c75f088602e21d6a73e9b50555b9ef4f
Matt Ricks <mgizmor37@gmail.com>
2019-03-11 23:02:33 -0600

    Add header data to server response, and customer index.html

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 
 //create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
+http.createServer(function(req, res) {
+    fs.readFile((__dirname + '/public/index.html'), { encoding: 'utf-8' }, function(err, data) {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/html' });
+            res.write('Server Error'); //write a response to the client
+            res.end(); //end the response
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data); //write a response to the client
+            res.end(); //end the response
+        }
+    });
 }).listen(8080); //the server object listens on port 8080
