14d6e227511d141b9ab69f7628eaa281a11f580f
Matt Ricks <mgizmor37@gmail.com>
2019-03-12 22:21:39 -0600

    Saving for the night

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 
+
+
 //create a server object:
 http.createServer(function(req, res) {
-    fs.readFile((__dirname + '/public/index.html'), { encoding: 'utf-8' }, function(err, data) {
+    fs.readFile((__dirname + '/circuits/index.html'), { encoding: 'utf-8' }, function(err, data) {
         if (err) {
             res.writeHead(500, { 'Content-Type': 'text/html' });
             res.write('Server Error'); //write a response to the client
@@ -11,6 +13,15 @@ http.createServer(function(req, res) {
         } else {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data); //write a response to the client
+            fs.readFile((__dirname + '/circuits/index.html'), { encoding: 'utf-8' }, function(err, data) {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/html' });
+                    res.write('Server Error'); //write a response to the client
+                    res.end(); //end the response
+                } else {
+
+                }
+            });
             res.end(); //end the response
         }
     });
